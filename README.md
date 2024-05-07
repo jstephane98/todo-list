a2c1878ffd2713b5700aca158552ef89db2c312e
Joel Kamo <kamostephane@gmail.com>
2024-05-07 17:10:01 +0100

    - Renommer une collection - Empêcher l'ajout d'une collection en doublon

--- README.md
+++ README.md
@@ -13,8 +13,8 @@
 ## Functionalities
 1. [x] Ajouter une collection
 2. [ ] Supprimer une collection
-3. [ ] Renommer une collection
-4. [ ] Empêcher l'ajout d'une collection en doublon
+3. [x] Renommer une collection
+4. [x] Empêcher l'ajout d'une collection en doublon
 5. [ ] Ajouter une tâche (reliée à une collection)
 6. [ ] Supprimer une tâche
 7. [ ] Afficher les taches d'une collection
--- tasks/views.py
+++ tasks/views.py
@@ -1,13 +1,22 @@
 from django.http import HttpResponse
-from django.shortcuts import render,  redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils.html import escape
 
-from tasks.models import Collection
+from tasks.models import Collection, Task
 
 
 # Create your views here.
 def index(request):
-    context = {'collections': Collection.objects.order_by('name')}
+    context = {}
+
+    collection_slug = request.GET.get('collection')
+
+    collection = Collection.get_default_collection()
+    if collection_slug:
+        collection = get_object_or_404(Collection, slug=collection_slug)
+
+    context['collections'] = Collection.objects.order_by('slug')
+    context['tasks'] = collection.task_set.order_by('description')
 
     return render(request, 'tasks/index.html', context=context)
 
@@ -16,7 +25,23 @@ def add_collection(request):
     collection_name = escape(request.POST.get('collection_name'))
     collection, created = Collection.objects.get_or_create(name=collection_name)
 
-    if created:
-        return HttpResponse('La collection existe déjà')
+    if not created:
+        return HttpResponse('La collection existe déjà', status=409)
 
-    return redirect('home')
+    return HttpResponse(f"<h2>{ collection_name }</h2>")
+
+
+def add_task(request):
+    collection = Collection.get_default_collection()
+
+    description = escape(request.POST.get('task-description'))
+    Task.objects.create(description=description, collection=collection)
+
+    return HttpResponse(description)
+
+
+def get_tasks(request, collection_pk: int):
+    collection = get_object_or_404(Collection, pk=collection_pk)
+    tasks = collection.task_set.order_by('description')
+    return render(request, 'tasks/tasks.html', context={'tasks': tasks})
+
